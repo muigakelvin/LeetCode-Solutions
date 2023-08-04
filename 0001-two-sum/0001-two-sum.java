4fb2c7b726e3e589272d17ae48b1119247e0db47
muigakelvin <kelvinkimondomuiga@gmail.com>
2023-08-04 22:04:08 +0300

    Time: 4 ms (65.08%), Space: 43.4 MB (94.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+    public int[] twoSum(int[] nums, int target) 
+    {
+        try {
+            Map<Integer, Integer> nummap = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) 
+            {
+                nummap.put(nums[i], i);
+            }
+            for (int i = 0; i < nums.length; i++) 
+            {
+                int rem = target - nums[i];
+                if (nummap.containsKey(rem) && nummap.get(rem) != i)
+                {
+                    return new int[]{i, nummap.get(rem)};
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
+
         return null;
     }
 }
